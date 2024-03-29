b13b5e7b3d7e12e2d2ad65a3152629cb2c9657b5
gjz010 <gjz010944@gmail.com>
2024-03-30 00:31:05 +0800

    fix: use patched unionmount and hfsnotify

--- flake.nix
+++ flake.nix
@@ -23,8 +23,10 @@
     heist-extra.url = "github:srid/heist-extra";
     heist-extra.flake = false;
 
-    unionmount.url = "github:srid/unionmount";
+    unionmount.url = "github:gjz010-Forks/unionmount";
     unionmount.flake = false;
+    hfsnotify.url = "github:gjz010-Forks/hfsnotify";
+    hfsnotify.flake = false;
 
     commonmark-simple.url = "github:srid/commonmark-simple";
     commonmark-simple.flake = false;
@@ -72,7 +74,8 @@
             unionmount.source = inputs.unionmount;
             commonmark-simple.source = inputs.commonmark-simple;
             commonmark-wikilink.source = inputs.commonmark-wikilink;
-            fsnotify.source = "0.4.1.0"; # Not in nixpkgs, yet.
+            fsnotify.source = inputs.hfsnotify;
+            #fsnotify.source = "0.4.1.0"; # Not in nixpkgs, yet.
             ghcid.source = "0.8.8";
             heist-extra.source = inputs.heist-extra;
           };
